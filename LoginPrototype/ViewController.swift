fb573070367e6bee850b54c9c4691149f8f73166
Marvin Kleinpass <marvin.kl@icloud.com>
2020-04-11 19:08:46 +0200

    Initial Commit

---
+++ LoginPrototype/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginPrototype
+//
+//  Created by Marvin Kleinpaß on 11.04.20.
+//  Copyright © 2020 Marvin Kleinpaß. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
